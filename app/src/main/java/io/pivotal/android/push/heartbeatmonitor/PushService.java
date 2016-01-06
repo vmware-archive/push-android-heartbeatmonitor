cfcc1473b8c6955d9182bc352b1646c0f64a804d
Rob Szumlakowski <rszumlakowski@pivotal.io>
2016-01-06 11:41:26 -0500

    AhmedM/RobS [#111148704] Changed the heartbeat monitor app to use the new onReceiveHeartbeat method in PCF Push 1.5.0
    Signed-off-by: Ernst Riemer <eriemer@pivotal.io>

--- app/src/main/java/io/pivotal/android/push/heartbeatmonitor/PushService.java
+++ app/src/main/java/io/pivotal/android/push/heartbeatmonitor/PushService.java
@@ -8,30 +8,14 @@ import io.pivotal.android.push.service.GcmService;
 
 public class PushService extends GcmService {
 
-    private static final String PCF_PUSH_HEARTBEAT = "pcf.push.heartbeat";
-
     @Override
-    public void onReceiveMessage(Bundle payload) {
-        if (isHeartbeat(payload)) {
-            Log.i(Const.LOG_TAG, "Received heartbeat.");
+    public void onReceiveHeartbeat(Bundle payload) {
+        Log.i(Const.LOG_TAG, "Received heartbeat.");
 
-            Preferences.tickHeartbeatCounter(this);
-            Preferences.tickLastTimestamp(this);
+        Preferences.tickHeartbeatCounter(this);
+        Preferences.tickLastTimestamp(this);
 
-            final Intent intent = new Intent(MainActivity.HEARTBEAT_RECEIVED);
-            sendBroadcast(intent);
-        }
-    }
-
-    private boolean isHeartbeat(final Bundle payload) {
-        if (payload == null) { return false; }
-
-        for (final String key : payload.keySet()) {
-            if (key.startsWith(PCF_PUSH_HEARTBEAT)) {
-                return true;
-            }
-        }
-
-        return false;
+        final Intent intent = new Intent(MainActivity.HEARTBEAT_RECEIVED);
+        sendBroadcast(intent);
     }
 }
