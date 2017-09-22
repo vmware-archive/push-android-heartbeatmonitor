f5f988ec5e8cd47b74afbb45cd0e130b3c99939e
Jason Smith <jsmith@pivotal.io>
2017-09-22 15:29:22 -0400

    [#151214384] Updated project to conform to Pivotal open source guidelines - Updated copyright dates - Added notices to build.gradle files

--- app/src/baidu/java/io/pivotal/android/push/heartbeatmonitor/receiver/PushReceiver.java
+++ app/src/baidu/java/io/pivotal/android/push/heartbeatmonitor/receiver/PushReceiver.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2014-Present Pivotal Software, Inc. All rights reserved.
+ *  Copyright (C) 2015-Present Pivotal Software, Inc. All rights reserved.
  *
  *  This program and the accompanying materials are made available under
  *  the terms of the under the Apache License, Version 2.0 (the "License”);
