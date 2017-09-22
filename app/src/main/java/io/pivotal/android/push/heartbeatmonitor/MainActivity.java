5f5f44f4f02cdd4012c6771cad7f52f92bafd38a
Jason Smith <jsmith@pivotal.io>
2017-09-22 15:07:04 -0400

    [#151214384] Updated project to conform to Pivotal open source guidelines
    - Added copyright notices to source code
    - Updated LICENCE and NOTICE document
    - Reformatted code to be consistent with team style guide
    - Updated Push SDK version to 1.7.1

--- app/src/main/java/io/pivotal/android/push/heartbeatmonitor/MainActivity.java
+++ app/src/main/java/io/pivotal/android/push/heartbeatmonitor/MainActivity.java
@@ -1,17 +1,18 @@
-//  Copyright (C) 2016 Pivotal Software, Inc. All rights reserved. 
-//  
-//  This program and the accompanying materials are made available under 
-//  the terms of the under the Apache License, Version 2.0 (the "License”); 
-//  you may not use this file except in compliance with the License. 
-//  You may obtain a copy of the License at
-//  
-//  http://www.apache.org/licenses/LICENSE-2.0
-//  
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
+/*  Copyright (C) 2015-Present Pivotal Software, Inc. All rights reserved. 
+ *
+ *  This program and the accompanying materials are made available under 
+ *  the terms of the under the Apache License, Version 2.0 (the "License”); 
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at 
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+  *  Unless required by applicable law or agreed to in writing, software 
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and 
+ * limitations under the License.
+  */
 
 package io.pivotal.android.push.heartbeatmonitor;
 
@@ -32,214 +33,214 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import com.google.common.collect.ImmutableSet;
-
+import io.pivotal.android.push.Push;
+import io.pivotal.android.push.prefs.Pivotal;
+import io.pivotal.android.push.registration.RegistrationListener;
 import java.net.URI;
 import java.util.Date;
 import java.util.List;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-import io.pivotal.android.push.Push;
-import io.pivotal.android.push.prefs.Pivotal;
-import io.pivotal.android.push.registration.RegistrationListener;
-
 public class MainActivity extends AppCompatActivity {
 
-    public static final String HEARTBEAT_RECEIVED = "io.pivotal.android.push.heartbeatmonitor.HEARTBEAT_RECEIVED";
-    public static final int TIMESTAMP_UPDATE_INTERVAL = 20000;
+  public static final String HEARTBEAT_RECEIVED = "io.pivotal.android.push.heartbeatmonitor.HEARTBEAT_RECEIVED";
+  public static final int TIMESTAMP_UPDATE_INTERVAL = 20000;
 
-    @Bind(R.id.heart)
-    ImageView heartView;
+  @Bind(R.id.heart)
+  ImageView heartView;
 
-    @Bind(R.id.toolbar)
-    Toolbar toolbar;
+  @Bind(R.id.toolbar)
+  Toolbar toolbar;
 
-    @Bind({R.id.text1, R.id.text2, R.id.text3})
-    List<TextView> textViews;
+  @Bind({R.id.text1, R.id.text2, R.id.text3})
+  List<TextView> textViews;
 
-    private BroadcastReceiver receiver;
-    private boolean isRegistered = false;
-    private Handler handler;
+  private BroadcastReceiver receiver;
+  private boolean isRegistered = false;
+  private Handler handler;
+  private Runnable updateTask = new Runnable() {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        ButterKnife.bind(this);
-
-        setSupportActionBar(toolbar);
+    public void run() {
+      updateHeartbeatCounter();
+      handler.postDelayed(updateTask, TIMESTAMP_UPDATE_INTERVAL);
     }
+  };
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        startPushRegistration();
-        startUpdateTask();
+    ButterKnife.bind(this);
 
-        setTitle(R.string.app_name);
+    setSupportActionBar(toolbar);
+  }
 
-        final IntentFilter intentFilter = new IntentFilter(HEARTBEAT_RECEIVED);
-        receiver = new BroadcastReceiver() {
+  @Override
+  protected void onResume() {
+    super.onResume();
+
+    startPushRegistration();
+    startUpdateTask();
+
+    setTitle(R.string.app_name);
+
+    final IntentFilter intentFilter = new IntentFilter(HEARTBEAT_RECEIVED);
+    receiver = new BroadcastReceiver() {
+
+      @Override
+      public void onReceive(Context context, Intent intent) {
+        runOnUiThread(new Runnable() {
+          @Override
+          public void run() {
+            animateHeartbeat();
+            updateHeartbeatCounter();
+          }
+        });
+      }
+    };
+    registerReceiver(receiver, intentFilter);
+  }
+
+  private void startPushRegistration() {
+
+    try {
+
+      textViews.get(0).setText("");
+      textViews.get(1).setText("");
+      textViews.get(2).setText(R.string.registering);
+
+      final ImmutableSet<String> tags = ImmutableSet.of("pcf.push.heartbeat");
+      Push.getInstance(this)
+          .startRegistration(Build.MODEL, tags, false, new RegistrationListener() {
 
             @Override
-            public void onReceive(Context context, Intent intent) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        animateHeartbeat();
-                        updateHeartbeatCounter();
-                    }
-                });
-            }
-        };
-        registerReceiver(receiver, intentFilter);
-    }
+            public void onRegistrationComplete() {
+              Log.i(Const.LOG_TAG, "Registration with PCF Push successful. Device ID is: " + Push
+                  .getInstance(MainActivity.this).getDeviceUuid());
 
-    private void startPushRegistration() {
-
-        try {
-
-            textViews.get(0).setText("");
-            textViews.get(1).setText("");
-            textViews.get(2).setText(R.string.registering);
-
-            final ImmutableSet<String> tags = ImmutableSet.of("pcf.push.heartbeat");
-            Push.getInstance(this).startRegistration(Build.MODEL, tags, false, new RegistrationListener() {
+              runOnUiThread(new Runnable() {
 
                 @Override
-                public void onRegistrationComplete() {
-                    Log.i(Const.LOG_TAG, "Registration with PCF Push successful. Device ID is: " + Push.getInstance(MainActivity.this).getDeviceUuid());
-
-                    runOnUiThread(new Runnable() {
-
-                        @Override
-                        public void run() {
-                            isRegistered = true;
-                            final String serviceUrl = Pivotal.getServiceUrl(MainActivity.this);
-                            final URI uri = URI.create(serviceUrl);
-                            textViews.get(2).setText(getString(R.string.monitoring, uri.getHost()));
-                            updateHeartbeatCounter();
-                        }
-                    });
+                public void run() {
+                  isRegistered = true;
+                  final String serviceUrl = Pivotal.getServiceUrl(MainActivity.this);
+                  final URI uri = URI.create(serviceUrl);
+                  textViews.get(2).setText(getString(R.string.monitoring, uri.getHost()));
+                  updateHeartbeatCounter();
                 }
+              });
+            }
+
+            @Override
+            public void onRegistrationFailed(final String s) {
+              Log.e(Const.LOG_TAG, "Registration with PCF Push failed: " + s);
+              runOnUiThread(new Runnable() {
 
                 @Override
-                public void onRegistrationFailed(final String s) {
-                    Log.e(Const.LOG_TAG, "Registration with PCF Push failed: " + s);
-                    runOnUiThread(new Runnable() {
-
-                        @Override
-                        public void run() {
-                            textViews.get(0).setText("");
-                            textViews.get(1).setText(R.string.registration_error);
-                            textViews.get(2).setText(s);
-                        }
-                    });
+                public void run() {
+                  textViews.get(0).setText("");
+                  textViews.get(1).setText(R.string.registration_error);
+                  textViews.get(2).setText(s);
                 }
-            });
-
-        } catch (Exception e) {
-
-            Log.e(Const.LOG_TAG, "Exception registering for PCF Push: " + e.getLocalizedMessage());
-            textViews.get(1).setText(R.string.registration_error);
-            textViews.get(0).setText("");
-            if (e.getLocalizedMessage() != null) {
-                textViews.get(2).setText(e.getLocalizedMessage());
-            } else {
-                textViews.get(2).setText(e.toString());
+              });
             }
-        }
+          });
+
+    } catch (Exception e) {
+
+      Log.e(Const.LOG_TAG, "Exception registering for PCF Push: " + e.getLocalizedMessage());
+      textViews.get(1).setText(R.string.registration_error);
+      textViews.get(0).setText("");
+      if (e.getLocalizedMessage() != null) {
+        textViews.get(2).setText(e.getLocalizedMessage());
+      } else {
+        textViews.get(2).setText(e.toString());
+      }
+    }
+  }
+
+  private void startUpdateTask() {
+    handler = new Handler();
+    handler.postDelayed(updateTask, TIMESTAMP_UPDATE_INTERVAL);
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+
+    unregisterReceiver(receiver);
+    heartView = null;
+
+    if (handler != null) {
+      handler.removeCallbacks(updateTask);
+    }
+    handler = null;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
     }
 
-    private void startUpdateTask() {
-        handler = new Handler();
-        handler.postDelayed(updateTask, TIMESTAMP_UPDATE_INTERVAL);
+    return super.onOptionsItemSelected(item);
+  }
+
+  @OnClick(R.id.heart)
+  public void onHeartTouched() {
+    animateHeartbeat();
+  }
+
+  private void animateHeartbeat() {
+    if (heartView != null) {
+      final Animation heartbeat = AnimationUtils.loadAnimation(this, R.anim.heartbeat);
+      heartView.startAnimation(heartbeat);
     }
+  }
 
-    private Runnable updateTask = new Runnable() {
+  private void updateHeartbeatCounter() {
 
-        @Override
-        public void run() {
-            updateHeartbeatCounter();
-            handler.postDelayed(updateTask, TIMESTAMP_UPDATE_INTERVAL);
-        }
-    };
+    final int heartbeatCounter = Preferences.getHeartbeatCounter(this);
+    final long latestTimestamp = Preferences.getLastTimestamp(this);
 
-    @Override
-    protected void onPause() {
-        super.onPause();
+    if (isRegistered) {
 
-        unregisterReceiver(receiver);
-        heartView = null;
+      if (latestTimestamp > 0) {
+        textViews.get(0).setText(getFormattedCounterScreen(heartbeatCounter));
+        setTimestampInTextView(latestTimestamp);
+      } else {
+        textViews.get(0).setText("");
+        textViews.get(1).setText(getFormattedCounterScreen(heartbeatCounter));
+      }
 
-        if (handler != null) {
-            handler.removeCallbacks(updateTask);
-        }
-        handler = null;
+    } else {
+      textViews.get(0).setText("");
+      textViews.get(1).setText("");
     }
+  }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
+  private void setTimestampInTextView(final long timestamp) {
+    final long difference = new Date().getTime() - timestamp;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    if (difference < 60000) {
+      textViews.get(1).setText(getString(R.string.last_timestamp_less_than_minute_ago));
+    } else {
+      textViews.get(1).setText(
+          getString(R.string.last_timestamp, DateUtils.getRelativeTimeSpanString(timestamp)));
     }
+  }
 
-    @OnClick(R.id.heart)
-    public void onHeartTouched() {
-        animateHeartbeat();
-    }
-
-    private void animateHeartbeat() {
-        if (heartView != null) {
-            final Animation heartbeat = AnimationUtils.loadAnimation(this, R.anim.heartbeat);
-            heartView.startAnimation(heartbeat);
-        }
-    }
-
-    private void updateHeartbeatCounter() {
-
-        final int heartbeatCounter = Preferences.getHeartbeatCounter(this);
-        final long latestTimestamp = Preferences.getLastTimestamp(this);
-
-        if (isRegistered) {
-
-            if (latestTimestamp > 0) {
-                textViews.get(0).setText(getFormattedCounterScreen(heartbeatCounter));
-                setTimestampInTextView(latestTimestamp);
-            } else {
-                textViews.get(0).setText("");
-                textViews.get(1).setText(getFormattedCounterScreen(heartbeatCounter));
-            }
-
-        } else {
-            textViews.get(0).setText("");
-            textViews.get(1).setText("");
-        }
-    }
-
-    private void setTimestampInTextView(final long timestamp) {
-        final long difference = new Date().getTime() - timestamp;
-
-        if (difference < 60000) {
-            textViews.get(1).setText(getString(R.string.last_timestamp_less_than_minute_ago));
-        } else {
-            textViews.get(1).setText(getString(R.string.last_timestamp, DateUtils.getRelativeTimeSpanString(timestamp)));
-        }
-    }
-
-    @NonNull
-    private String getFormattedCounterScreen(int heartbeatCounter) {
-        return getResources().getQuantityString(R.plurals.heartbeat_counter, heartbeatCounter, heartbeatCounter);
-    }
+  @NonNull
+  private String getFormattedCounterScreen(int heartbeatCounter) {
+    return getResources()
+        .getQuantityString(R.plurals.heartbeat_counter, heartbeatCounter, heartbeatCounter);
+  }
 }
--- app/src/main/java/io/pivotal/android/push/heartbeatmonitor/Preferences.java
+++ app/src/main/java/io/pivotal/android/push/heartbeatmonitor/Preferences.java
@@ -1,61 +1,61 @@
-//  Copyright (C) 2016 Pivotal Software, Inc. All rights reserved. 
-//  
-//  This program and the accompanying materials are made available under 
-//  the terms of the under the Apache License, Version 2.0 (the "License”); 
-//  you may not use this file except in compliance with the License. 
-//  You may obtain a copy of the License at
-//  
-//  http://www.apache.org/licenses/LICENSE-2.0
-//  
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
+/*  Copyright (C) 2015-Present Pivotal Software, Inc. All rights reserved. 
+ *
+ *  This program and the accompanying materials are made available under 
+ *  the terms of the under the Apache License, Version 2.0 (the "License”); 
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at 
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+  *  Unless required by applicable law or agreed to in writing, software 
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and 
+ * limitations under the License.
+  */
 
 package io.pivotal.android.push.heartbeatmonitor;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-
 import java.util.Date;
 
 public class Preferences {
 
-    public static final String HEARTBEAT_COUNTER = "heartbeat_counter";
-    public static final String HEARTBEAT_LAST_TIMESTAMP = "heartbeat_last_timestamp";
+  public static final String HEARTBEAT_COUNTER = "heartbeat_counter";
+  public static final String HEARTBEAT_LAST_TIMESTAMP = "heartbeat_last_timestamp";
 
-    public static int getHeartbeatCounter(final Context context) {
-        return getSharedPreferences(context).getInt(HEARTBEAT_COUNTER, 0);
-    }
+  public static int getHeartbeatCounter(final Context context) {
+    return getSharedPreferences(context).getInt(HEARTBEAT_COUNTER, 0);
+  }
 
-    public static void setHeartbeatCounter(final Context context, final int counter) {
-        final SharedPreferences.Editor editor = getSharedPreferences(context).edit();
-        editor.putInt(HEARTBEAT_COUNTER, counter);
-        editor.apply();
-    }
+  public static void setHeartbeatCounter(final Context context, final int counter) {
+    final SharedPreferences.Editor editor = getSharedPreferences(context).edit();
+    editor.putInt(HEARTBEAT_COUNTER, counter);
+    editor.apply();
+  }
 
-    public static void tickHeartbeatCounter(final Context context) {
-        final int heartbeatCounter = getHeartbeatCounter(context);
-        setHeartbeatCounter(context, heartbeatCounter + 1);
-    }
+  public static void tickHeartbeatCounter(final Context context) {
+    final int heartbeatCounter = getHeartbeatCounter(context);
+    setHeartbeatCounter(context, heartbeatCounter + 1);
+  }
 
-    public static long getLastTimestamp(final Context context) {
-        return getSharedPreferences(context).getLong(HEARTBEAT_LAST_TIMESTAMP, 0);
-    }
+  public static long getLastTimestamp(final Context context) {
+    return getSharedPreferences(context).getLong(HEARTBEAT_LAST_TIMESTAMP, 0);
+  }
 
-    public static void setLastTimestamp(final Context context, final Date timestamp) {
-        final SharedPreferences.Editor editor = getSharedPreferences(context).edit();
-        editor.putLong(HEARTBEAT_LAST_TIMESTAMP, timestamp.getTime());
-        editor.apply();
-    }
+  public static void setLastTimestamp(final Context context, final Date timestamp) {
+    final SharedPreferences.Editor editor = getSharedPreferences(context).edit();
+    editor.putLong(HEARTBEAT_LAST_TIMESTAMP, timestamp.getTime());
+    editor.apply();
+  }
 
-    public static void tickLastTimestamp(final Context context) {
-        setLastTimestamp(context, new Date());
-    }
+  public static void tickLastTimestamp(final Context context) {
+    setLastTimestamp(context, new Date());
+  }
 
-    private static SharedPreferences getSharedPreferences(final Context context) {
-        return PreferenceManager.getDefaultSharedPreferences(context);
-    }
+  private static SharedPreferences getSharedPreferences(final Context context) {
+    return PreferenceManager.getDefaultSharedPreferences(context);
+  }
 }
